cc8ca25a200b76f7e8c4fd02b190a6697f41f1f1
Luke Davis <lukelbd@gmail.com>
2023-05-27 14:23:08 -0600

    Document bug fixing

--- .jupyter/jupyter_server_config.py
+++ .jupyter/jupyter_server_config.py
@@ -1,5 +1,8 @@
-# Enable code formatter to prevent popup message
+# Enable code formatter to prevent popup message.
 # See: https://github.com/ryantam626/jupyterlab_code_formatter/issues/193#issuecomment-808169451
+# However this did not actually work... instead had to downgrade
+# to older version with 'pip install jupyterlab-code-formatter==1.5.3'.
+# See: https://github.com/ryantam626/jupyterlab_code_formatter/issues/193#issuecomment-1488742233
 c.ServerApp.jpserver_extensions = {
     'jupyterlab': True,
     'jupyterlab_code_formatter': True,
